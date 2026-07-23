76d51e003d3b677b6ca226b03402cb204473cb84
Baoqiang <baoqiangy@gmail.com>
2026-07-23 00:23:27 -0500

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,1 @@
+Open index.html in a web browser. All files must remain in the same folder. The site is printable and works offline.
---
+++ site.js
@@ -0,0 +1,1 @@
+document.addEventListener('DOMContentLoaded',()=>{document.querySelectorAll('pre').forEach(p=>{p.title='Cisco IOS commands';});});
